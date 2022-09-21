ebb041abc50858cebdf1583bdc5fb8b5237405d9
hedi <hedi@DESKTOP-MG5UJB9>
2022-09-21 13:51:25 +0430

    Add project files.

---
+++ Abstract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass2 c2 = new Myclass2();
+            c2.car();
+            Console.ReadKey();
+        }
+    }
+    public abstract class Myclass1
+    {
+        public void car()
+        {
+            Console.WriteLine("Car Body...");
+        }
+    }
+    public class Myclass2:Myclass1
+    {
+
+    }
+}
